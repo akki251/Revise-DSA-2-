17856019b04be343b94f23097f06bff5d12efcee
Akshansh Shrivastava <akshansh773@gmail.com>
2023-04-01 10:03:20 +0530

    Update controller algo

--- server/controllers/homeControllers.js
+++ server/controllers/homeControllers.js
@@ -56,7 +56,7 @@ function fetchQuestions(tag, problems) {
       let oldDate = new Date('1970-01-01T00:00:00.000Z');
       problems.forEach((prob) => {
         let milliseconds = prob.revisionFreq * 24 * 60 * 60 * 1000;
-        if (prob.tag === tag && date - prob.date > milliseconds && prob.date - oldDate != 0) {
+        if (prob.tag === tag && date - prob.date > milliseconds && prob.date - oldDate === 0) {
           arrayQ.push(prob);
         }
       });
